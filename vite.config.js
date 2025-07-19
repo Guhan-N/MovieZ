2ccff1e43073c9c5e0fb21db07f02bbe24036ca3
Guhan <guhan6575@gmail.com>
2025-07-19 19:20:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 // vite.config
 import { defineConfig } from 'vite';
+import { createClient } from '@supabase/supabase-js';
 
 export default defineConfig({
   base: '/MovieZ/', // must match your repo name
