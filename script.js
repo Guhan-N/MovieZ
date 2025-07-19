c63e0af0b01f831ee9a40e2d030afc8a8356198a
Guhan <guhan6575@gmail.com>
2025-07-19 18:41:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY; // Change this line
-const API_BASE_URL = import.meta.env.VITE_TMDB_BASE_URL || 'https://ofzplydqwuuxnxtsymbl.supabase.co'
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY || '44586f891977911675d186d6e794d464';
+const API_BASE_URL = import.meta.env.VITE_TMDB_BASE_URL || 'https://ofzplydqwuuxnxtsymbl.supabase.co';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 const BACKDROP_BASE_URL = 'https://image.tmdb.org/t/p/original';
 
